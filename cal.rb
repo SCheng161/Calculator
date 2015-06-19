680cc3ab018129e60f1e7adefe5a1e198902e653
Scheng161 <Scheng161@gmail.com>
2015-06-19 19:02:23 +0000

    Hi

--- cal.rb
+++ cal.rb
@@ -1,9 +1,9 @@
-Hi 
-git add .
-  git commit -m " "
-git push 
+# git add .
+#   git commit -m " "
+# git push 
 
-git pull
-dfhufhwedu
+# git pull
 
-hey there 
+puts "Welcome to Lucy and Sarah's Awesome Calculator!"
+puts "For addition type 1; For subtraction type 2; For multiplication type 3; For division type 4."
+
