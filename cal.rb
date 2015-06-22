ced9e39c94242717e58070b1eff8c7b602961908
lucypoteshman <lucy.polarbear@gmail.com>
2015-06-22 13:18:53 +0000

    aoiszldjfkaesoidfl

--- cal.rb
+++ cal.rb
@@ -5,20 +5,37 @@
 # git pull
 
 puts "Welcome to Lucy and Sarah's Awesome Calculator!"
-puts "For addition type 1; For subtraction type 2; For multiplication type 3; For division type 4." 
+puts "For addition type 1; For subtraction type 2; For multiplication type 3; For division type 4; For squaring type 5." 
 key = gets.chomp
-if key = "1" 
+if key == "1" 
   then puts "Enter first number"
     num1 = gets.chomp.to_i
     puts "Enter second number"
     num2 = gets.chomp.to_i
     puts num1 + num2
-  elsif key = "2" 
+  elsif key == "2" 
     then puts "Enter first number"
-    num3 = gets.chomp.to_i
+    num1 = gets.chomp.to_i
     puts "Enter second number"
-    num4 = gets.chomp.to_i
+    num2 = gets.chomp.to_i
     puts num1 - num2
+        elsif key == "3" 
+    then puts "Enter first number"
+    num1 = gets.chomp.to_i
+    puts "Enter second number"
+    num2 = gets.chomp.to_i
+    puts num1 * num2
+       elsif key == "4" 
+    then puts "Enter first number"
+    num1 = gets.chomp.to_i
+    puts "Enter second number"
+    num2 = gets.chomp.to_i
+    puts num1 / num2
+        elsif key == "5" 
+    then puts "Enter first number"
+    num1 = gets.chomp.to_i
+      puts num1 * num1
+    puts num1
   else puts "dsfwef"
   end
     
